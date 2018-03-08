fd566f3d8ae5021c62eb81af4e8b4d298d910e3e
Roudolpho <roudolphigalous@yahoo.com>
2018-03-08 12:47:32 -0500

    Math2 min max

--- Math2.java
+++ Math2.java
@@ -17,4 +17,10 @@ public class Math2
     public static double radToDeg(double rad) {
         return (double) rad * (180*Math.PI);
     }
+    public static int max(int a, int b) {
+        if(a>b){return a;} else {return b;}
+    }
+    public static int min(int a, int b) {
+        if(a<b){return a;} else {return b;}
+    }
 }
